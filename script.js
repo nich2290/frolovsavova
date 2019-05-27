e2652f948f2b6897ff605526cd990b83ca984044
Nicklas Vilhelmsen <nicklasvilhelmsen@overmind-the3rd.kea.local>
2019-05-27 15:48:17 +0200

    styling-tilbud

--- script.js
+++ script.js
@@ -16,21 +16,4 @@ $(document).ready(
     }
 )
 
-document.querySelector(".container1").addEventListener("click", popUps);
-document.querySelector(".container2").addEventListener("click", popUps2);
-document.querySelector(".container3").addEventListener("click", popUps3);
 
-function popUps() {
-    console.log("AKTIV")
-    document.querySelector(".popup1").classList.toggle("display");
-}
-
-function popUps2() {
-    console.log("AKTIV")
-    document.querySelector(".popup2").classList.toggle("display");
-}
-
-function popUps3() {
-    console.log("AKTIV")
-    document.querySelector(".popup3").classList.toggle("display");
-}
