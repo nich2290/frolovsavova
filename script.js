6412d0a7f6962b7b8299214e457650e5ad862bc2
Oliver <oliv5773@stud.kea.dk>
2019-05-27 15:40:58 +0200

    navmenu pimpet (får farve ved scroll) - stadig forkerte farver

--- script.js
+++ script.js
@@ -15,3 +15,17 @@ $(document).ready(
 
     }
 )
+
+/*Navigationsmenu - transparent før scroll */
+
+$(document).ready(function () {
+    // Transition effect for navbar
+    $(window).scroll(function () {
+        // checks if window is scrolled more than 500px, adds/removes solid class
+        if ($(this).scrollTop() > 1) {
+            $('.navbar').addClass('solid');
+        } else {
+            $('.navbar').removeClass('solid');
+        }
+    });
+});
